2eb52dbc60682b509e98e44de2652222bb316186
rafmoshe2500 <rafmoshe2500@gmail.com>
2024-02-09 01:16:53 +0200

    add animation to edit itemds

--- app/src/main/java/com/example/shopease/wishLists/ShopItemOptionsBottomSheetDialogFragment.kt
+++ app/src/main/java/com/example/shopease/wishLists/ShopItemOptionsBottomSheetDialogFragment.kt
@@ -35,7 +35,10 @@ class ShopItemOptionsBottomSheetDialogFragment : DialogFragment() {
             return fragment
         }
     }
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
